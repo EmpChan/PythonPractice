4c3e0b79a237bf8990d58c27ed52d6990a0c6d08
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-03-28 13:38:51 +0900

    Create code4.py

---
+++ 4주차실습/code4.py
@@ -0,0 +1,17 @@
+"""
+이름 : 황재찬
+학번 : 2020039040
+"""
+
+aa = []
+bb = []
+value = 0
+
+for i in range(0, 100):
+    aa.append(value)
+    value+=2
+
+for i in range(0,100):
+    bb.append(aa[99-i])
+
+print("bb[0]에는 %d이, bb[99]에는 %d이 입력됩니다."%(bb[0],bb[99]))
