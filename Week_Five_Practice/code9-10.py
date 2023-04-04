6535a2bf96007621fbd7f2c4a709e4f251218473
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-04-04 15:23:21 +0900

    Create code9-10.py

---
+++ Week_Five_Practice/code9-10.py
@@ -0,0 +1,13 @@
+"""
+이름:황재찬
+학번:2020039040
+"""
+
+def para2_func(v1,v2):
+    return v1+v2
+
+def para3_func(v1,v2,v3):
+    return v1+v2+v3
+
+print("매개변수가 2개인 함수를 호출한 결과 ==>",para2_func(10,20))
+print("매개변수가 3개인 함수를 호출한 결과 ==>",para3_func(10, 20, 30))c
