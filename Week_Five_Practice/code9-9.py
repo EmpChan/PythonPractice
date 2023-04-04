3caec7c89603d0828b8d92a56bcb75183202ea43
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-04-04 15:21:02 +0900

    Create code9-9.py

---
+++ Week_Five_Practice/code9-9.py
@@ -0,0 +1,12 @@
+"""
+이름:황재찬
+학번:2020039040
+"""
+
+def multi(v1,v2):
+    retList = [v1+v2,v1-v2]
+    return retList
+
+myList = multi(100,200)
+print("multi()에서 돌려준 값 ==>",myList[0],",",myList[1])
+
