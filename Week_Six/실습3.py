fd5c44d00d2af5c8bc20b4a1991fa90cc6987b9b
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-04-11 23:37:21 +0900

    Create 실습3.py

---
+++ Week_Six/실습3.py
@@ -0,0 +1,15 @@
+"""
+이름 : 황재찬
+학번 : 2020039040
+"""
+def recur(n: int) ->int:
+    if n>0:
+        recur(n-1)
+        print(n)
+        recur(n-2)
+
+x = int(input('정숫값을 입력하세요:'))
+
+recur(x)
+
+
