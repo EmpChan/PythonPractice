a03926c005f099d3e1e5bc0ac78690a55cc335aa
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-04-11 23:14:19 +0900

    Create 실습2.py

---
+++ Week_Six/실습2.py
@@ -0,0 +1,17 @@
+"""
+이름 : 황재찬
+학번 : 2020039040
+"""
+def gcd(x: int, y:int)->int:
+    if y==0:
+        return x
+    else:
+        return gcd(y,x%y)
+
+if __name__ == '__main__':
+    print('두 정수값의 최대 공약수를 구합니다.')
+    x = int(input('첫 번째 정숫값을 입력하세요. :'))
+    y = int(input('두 번째 정숫값을 입력하세요. :'))
+
+    print(f'두 정숫값의 최대 공약수는{gcd(x,y)}입니다.')
+    
