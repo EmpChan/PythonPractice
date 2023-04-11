79f381b0dc8fbd8d8c4e31fa29ab6f69e1fb62c9
JaeChanHWANG <57826563+OopsIForgot@users.noreply.github.com>
2023-04-11 14:06:11 +0900

    Create code 11-16.py

---
+++ Week_Six_Practice/code 11-16.py
@@ -0,0 +1,19 @@
+"""
+이름 : 황재찬
+학번 : 2020039040
+"""
+num1 = input('숫자1 -->')
+num2 = input('숫자2 -->')
+
+try:
+    num1 = int(num1)
+    num2 = int(num2)
+
+except:
+    print('오류가 발생했습니다.')
+
+else:
+    print(num1, '/', num2, '=',num1/num2)
+
+finally:
+    print('이 부분은 무조건 나옵니다.')
